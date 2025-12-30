635bffbda9c5df2ece275c5f6f0cfd245d22fa25
Imhrutik05 <hrutikchaudhari5@gmail.com>
2025-12-30 14:38:43 +0530

    created a function to format the date

---
+++ client/src/utils/formatDate.js
@@ -0,0 +1,12 @@
+export function formatDate(isoString) {
+    if (!isoString) return "-";
+
+    const date = new Date(isoString);
+    if (isNaN(date.getTime())) return "Invalid Date";
+
+    return date.toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
+}
